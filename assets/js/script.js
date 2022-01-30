f8c44f381cc96a9a1b4963c9c8f73f42c0fdcee0
Isaac <you@example.com>
2022-01-30 13:39:13 +0200

    3rd js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,20 @@ function newQuote() {
   const randomQuote = Math.floor(Math.random() * apiQuotes.length);
   const quote = apiQuotes[randomQuote];
   authorText.textContent = quote.author;
+
+  // Check if Author field is blank and replace with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    quoteText.textContent = quote.author;
+  }
+  //   Check Quote length to determine styling
+  if (quote.text.length > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
   quoteText.textContent = quote.text;
 }
 
@@ -23,6 +37,16 @@ async function getQuotes() {
     newQuote();
   } catch (e) {}
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuotes();
