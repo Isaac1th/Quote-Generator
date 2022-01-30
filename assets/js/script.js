747ef7df8815b73264d35b5de705bfedd19eecb1
Isaac <you@example.com>
2022-01-30 09:37:09 +0200

    Integrate API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   const randomQuote = Math.floor(Math.random() * apiQuotes.length);
   const quote = apiQuotes[randomQuote];
-  console.log(quote);
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
